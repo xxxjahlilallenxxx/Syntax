454e04bafb5f741b9c851830c50d72aa302e8349
JA@02697400 <jahlil.allen@bison.howard.edu>
2019-03-24 12:45:57 -0400

    Completed all function definitions

--- syntax.c
+++ syntax.c
@@ -93,3 +93,62 @@ void addChar() {
   else
     printf("Error - lexeme is too long");
 }
+
+/**/
+void getChar(){
+  if ((nextChar = getc(in_fp)) != EOF) {
+    if (isalpha(nextChar))
+      charClass = LETTER;
+    else if (isdigit(nextChar))
+      charClass = DIGIT;
+    else
+      charClass = UNKNOWN;
+  }
+  else
+    charClass = EOF;
+}
+
+/**/
+void getNonBlank() {
+  while (isspace(nextChar))
+    getChar();
+}
+
+/**/
+int lex(){
+  lexLen = 0;
+  getNonBlank();
+  switch(charClass){
+    case LETTER:
+      addChar();
+      getChar();
+      while(charClass == LETTER || charClass == DIGIT){
+        addChar();
+        getChar();
+      }
+      nextToken = IDENT;
+      break;
+    case DIGIT:
+      addChar();
+      getChar();
+      while(charClass == DIGIT){
+        addChar();
+        getChar();
+      }
+      nextToken = INT_LIT;
+      break;
+    case UNKNOWN:
+      lookup(nextChar);
+      getChar();
+      break;
+    case EOF:
+      nextToken = EOF;
+      lexeme[0] = 'E';
+      lexeme[1] = 'O';
+      lexeme[2] = 'F';
+      lexeme[3] = 0;
+      break;
+    }
+    printf("Next token is: %d, Next lexeme is %s\n", nextToken, lexeme);
+    return nextToken;
+}
